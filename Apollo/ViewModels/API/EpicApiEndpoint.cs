8f5c134fb53761abaa40d298bb7b2b28adfdde69
GhostScissors <79089473+GhostScissors@users.noreply.github.com>
2025-01-14 14:41:08 +0530

    why epic games

--- Apollo/ViewModels/API/EpicApiEndpoint.cs
+++ Apollo/ViewModels/API/EpicApiEndpoint.cs
@@ -11,7 +11,7 @@ public class EpicApiEndpoint : AbstractApiProvider
     private const string AUTH_URL = "https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token";
     private const string MANFEST_URL = "https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/public/assets/v2/platform/Windows/namespace/fn/catalogItem/4fe75bbc5a674f4f9b356b5c90567da5/app/Fortnite/label/Live";
     private const string VERIFY_URL = "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/verify";
-    private const string BASIC_TOKEN = "basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=";
+    private const string BASIC_TOKEN = "basic ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ=";
     
     private string AuthToken { get; set; }
 
